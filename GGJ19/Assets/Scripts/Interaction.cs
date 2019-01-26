62dcd147d3b2211436e5e65ae46033939491964f
narval <isaaclpe@gmail.com>
2019-01-26 10:03:08 +0100

    Añade la lógica para los objetos interactuables

---
+++ GGJ19/Assets/Scripts/Interaction.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour {
+    #region Events
+    [HideInInspector] public UnityEvent OnGetClose;
+    [HideInInspector] public UnityEvent OnSelected;
+    [HideInInspector] public UnityEvent OnLeave;
+    public UnityEvent OnInteract;
+    #endregion
+
+    #region References
+    public PlayerIntaractor playerInteractor;
+    #endregion
+
+    #region Internal
+    public Color hightightedColor;
+    public bool oneTime;
+    public bool used;
+    public List<int> prerequisites;
+    #endregion
+
+    void Start() {
+        playerInteractor = FindObjectOfType<PlayerIntaractor>();
+
+        OnGetClose.AddListener(GetClose);
+        OnLeave.AddListener(GetFar);
+        OnSelected.AddListener(Selected);
+        OnInteract.AddListener(Interact);
+    }
+
+    void Update() {
+        
+    }
+
+    public void GetClose() {
+        GetComponent<MeshRenderer>().material.color = hightightedColor - (Color.grey * 2);
+    }
+
+    public void Selected() {
+        GetComponent<MeshRenderer>().material.color = hightightedColor;
+    }
+
+    public void GetFar() {
+        GetComponent<MeshRenderer>().material.color = Color.white;
+    }
+
+    public virtual void Interact() {
+        if (used && oneTime) { 
+            playerInteractor.PlayWrongSound();
+            Debug.Log("<Color=red>Already used " + name + "</Color>");
+            return;
+        }
+        foreach (var item in prerequisites) {
+            if (!playerInteractor.itemGot.Contains(item)) {
+                playerInteractor.PlayWrongSound();
+                Debug.Log("<Color=red>" + name + " not meeting condition " + item + "</Color>");
+                return;
+            }
+        }
+        playerInteractor.PlayCorrectSound();
+        used = true;
+        Debug.Log("<Color=blue>Used " + name + "</Color>");
+        if (oneTime) {
+            OnLeave.Invoke();
+            playerInteractor.closestInteraction = null;
+            playerInteractor.closeInteractions.Remove(this);
+        }
+    }
+}
